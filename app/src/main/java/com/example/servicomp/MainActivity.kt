986ed8ed053bdaf0799cfc48699cb35488ef11d4
Xnite-X <48861223+Xnite-X@users.noreply.github.com>
2024-05-12 13:59:09 +0700

    Buat Halaman intro, login, register

--- app/src/main/java/com/example/servicomp/MainActivity.kt
+++ app/src/main/java/com/example/servicomp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.servicomp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import com.example.servicomp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set OnClickListener for the login button
+        binding.btnLogin.setOnClickListener {
+            // Start the login activity
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        // Set OnClickListener for the register button
+        binding.btnRegist.setOnClickListener {
+            // Start the register activity
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/servicomp/RegisterActivity.kt
@@ -0,0 +1,5 @@
+package com.example.servicomp
+
+class RegisterActivity {
+
+}
